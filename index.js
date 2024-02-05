654f0c913703cfdc2329038a8ef2b79f0dc2ec2f
mcorusa <marinacorusa1@gmail.com>
2024-02-05 18:56:39 +0100

    advice number update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn");
 const adviceEl = document.getElementById("advice-content");
+const adviceNoEl = document.getElementById("advice-no");
 const apiURL = "https://api.adviceslip.com/advice";
 
 
@@ -12,11 +13,13 @@ async function getAdvice(){
         const response = await fetch(apiURL);
         const data = await response.json();
         const randomAdvice = data.slip.advice;
+        const idNo=data.slip.id;
+        adviceNoEl.innerText ="advice #"+idNo;
         adviceEl.innerText='"'+randomAdvice + '"';
         btnEl.disabled=false;
     }   catch(error){
             adviceEl.innerText="An error occured. Try again later!";
-            btnEl.disabled=false;
+           // btnEl.disabled=false;
         }
 
 
